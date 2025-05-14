bab3c8494275e11f33c8c58581ca99e86dd86f9b
Enes Türkhan <165434346+enesturkhan@users.noreply.github.com>
2025-05-14 17:18:05 +0300

    2323

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ['api.qrserver.com'],
+  },
+};
+
+export default nextConfig;
