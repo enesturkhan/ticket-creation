dc3afd8a0f18b3a91700f4acf996f0ee7a7490a4
Enes Türkhan <165434346+enesturkhan@users.noreply.github.com>
2025-05-15 15:09:40 +0300

    3

--- app/components/SelectField.tsx
+++ app/components/SelectField.tsx
@@ -37,7 +37,9 @@ export default function SelectField({
         id={id}
         required={required}
         value={value || ''}
-        onChange={(e: any) => onChange && onChange(e.target.value)}
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+          onChange && onChange(e.target.value)
+        }
         aria-invalid={error ? 'true' : 'false'}
         aria-describedby={error ? `${id}-error` : description ? `${id}-description` : undefined}
         className={`w-full px-4 py-2 border bg-white/20 ${
